685453c35aed69dde1d4d3e5577668b83e2152e9
Roberto Branni <rob.branni@gmail.com>
2026-02-03 12:40:16 +0100

    remove verce analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,9 +35,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`font-sans antialiased`}>
-				{children}
-			</body>
+			<body className={`font-sans antialiased`}>{children}</body>
 		</html>
 	);
 }
